b57123f2d0bf20230611a8c3ca10e6ac987767b5
John Hanley <jack@caged-media.com>
2013-02-18 06:48:32 -0800

    Tweaks

--- app/overrides/add_class_to_thumbnails.rb
+++ app/overrides/add_class_to_thumbnails.rb
@@ -1,5 +1,7 @@
+=begin
 Deface::Override.new(:virtual_path => %q{spree/products/_thumbnails},
                     :name => %q{replace_thumbnails},
                     :disabled => true,
                     :add_to_attributes => %q{li},
                     :attributes => {:class => 'span1'})
+=end
